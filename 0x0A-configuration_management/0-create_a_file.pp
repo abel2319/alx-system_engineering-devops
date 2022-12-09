5f79b03672a21800549156a20d9accdd70a23630
abel2319 <abelbakary@gmail.com>
2022-12-09 19:44:47 +0100

    task0 handle with puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+# Using Puppet, create a file in /tmp. file name scholl, permission 0744, owner zzz-data, group www-data, content 'I love Puppet'
+
 file { '/tmp/school':
-  mode => '0744'
-  owner => 'www-data'
-  group => 'www-data'
-  content => 'I love Puppet'
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
