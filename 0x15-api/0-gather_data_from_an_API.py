6b090be86488e261959e82deb3c806c80898fb2b
abel2319 <abelbakary@gmail.com>
2023-01-17 14:43:29 +0100

    task0.5

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,18 +3,19 @@
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    """entry"""
+    response = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                            argv[1])
+    response2 = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                             argv[1] + '/todos')
 
-response = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                        argv[1])
-response2 = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                         argv[1] + '/todos')
+    dico = response.json()
+    dico2 = response2.json()
 
-dico = response.json()
-dico2 = response2.json()
+    tasks_done = [task for task in dico2 if task.get('completed') is True]
 
-tasks_done = [task for task in dico2 if task.get('completed') is True]
+    print('Employee {} is done with tasks({}/{}):'
+          .format(dico.get('name'), len(tasks_done), len(dico2)))
 
-print('Employee {} is done with tasks({}/{}):'
-      .format(dico.get('name'), len(tasks_done), len(dico2)))
-
-for task in tasks_done: print('\t {}'.format(task.get('title')))
+    for task in tasks_done: print('\t {}'.format(task.get('title')))
