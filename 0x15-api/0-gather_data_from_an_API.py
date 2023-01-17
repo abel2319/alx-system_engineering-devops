cc253ed8500e2cbfdc44f0d26dac3bc4dc9400a2
abel2319 <abelbakary@gmail.com>
2023-01-17 08:40:08 +0100

    task0.1 pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-#Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
+"""Python script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress."""
 
 import requests
 from sys import argv
 
 
-response = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1])
-dico = response.json()
+response = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                        argv[1])
+response2 = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                         argv[1] + '/todos')
 
-response2 = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1] +'/todos')
+dico = response.json()
 dico2 = response2.json()
 
-tasks_done = [task for task in dico2 if task.get('completed') == True]
+tasks_done = [task for task in dico2 if task.get('completed') is True]
 
-print('Employee {} is done with tasks({}/{}):'.format(dico.get('name'), len(tasks_done), len(dico2)))
+print('Employee {} is done with tasks({}/{}):'
+      .format(dico.get('name'), len(tasks_done), len(dico2)))
+
 for task in tasks_done:
     print('\t {}'.format(task.get('title')))
