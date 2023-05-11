b89c5d6ff8142435e8bf6f0988e4c0a0e4a8c228
abel2319 <abelbakary@gmail.com>
2023-05-11 03:42:38 +0100

    initialization 2

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 #Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet (instead of using Bash as you were previously doing).
+#change phpp extension to php
 
 
 exec { 'fix 500 server error':
