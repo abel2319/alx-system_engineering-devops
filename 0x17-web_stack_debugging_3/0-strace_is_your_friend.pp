3e157f08c91ae5a9c1376d0ccf31a62e4ffb1e23
abel2319 <abelbakary@gmail.com>
2023-05-11 12:57:51 +0100

    fix0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-#Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet (instead of using Bash as you were previously doing).
+#Debug apache2 server using strace and tmux.
+#Problem found, phpp extemsion instead of php
 
 
 exec { 'fix 500 server error':
