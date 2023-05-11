1eda866c55b7d655212a944028020240615b584c
abel2319 <abelbakary@gmail.com>
2023-05-11 13:02:55 +0100

    fix1

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,7 @@
 #Problem found, phpp extemsion instead of php
 
 
-exec { 'fix 500 server error':
-  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path => '/usr/local/bin/:/bin/'
+exec {'fix 500 server error':
+	command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+	path => '/usr/local/bin/:/bin/'
 }
