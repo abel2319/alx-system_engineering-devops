2dda6125f06bf0ad9507c114eb505144fd80dd37
Abel KPOHINTO <93947022+abel2319@users.noreply.github.com>
2023-08-03 13:30:49 +0100

    Delete 0x17-web_stack_debugging_3 directory
    before restart

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,8 +0,0 @@
-#Debug apache2 server using strace and tmux.
-#Problem found, phpp extemsion instead of php
-
-
-exec {'fix 500 server error':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
