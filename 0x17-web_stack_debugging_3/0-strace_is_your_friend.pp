65f643864efd28e85cd514872d68d11d55aac994
abel2319 <abelbakary@gmail.com>
2023-08-10 03:20:34 +0100

    debugging 3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+#Debug apache2 server using strace and tmux.
+#Problem found, phpp extemsion instead of php
+
+
+exec {'fix 500 server error':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
